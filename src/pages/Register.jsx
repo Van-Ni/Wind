f7106b870b850a6621348ff84c193c0d11cf1bf4
An Pham <quocan01255@gmail.com>
2023-11-12 17:30:14 +0700

    Add login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,125 +6,148 @@ import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { registerRoute } from "../utils/APIRoutes";
+import '../assets/css/authStyle.css';
 
 export default function Register() {
-  const navigate = useNavigate();
-  const toastOptions = {
-    position: "bottom-right",
-    autoClose: 8000,
-    pauseOnHover: true,
-    draggable: true,
-    theme: "dark",
-  };
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  // const navigate = useNavigate();
+  // const toastOptions = {
+  //   position: "bottom-right",
+  //   autoClose: 8000,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   theme: "dark",
+  // };
+  // const [values, setValues] = useState({
+  //   username: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // });
 
-  useEffect(() => {
-    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-      navigate("/");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+  //     navigate("/");
+  //   }
+  // }, []);
 
-  const handleChange = (event) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
+  // const handleChange = (event) => {
+  //   setValues({ ...values, [event.target.name]: event.target.value });
+  // };
 
-  const handleValidation = () => {
-    const { password, confirmPassword, username, email } = values;
-    if (password !== confirmPassword) {
-      toast.error(
-        "Password and confirm password should be same.",
-        toastOptions
-      );
-      return false;
-    } else if (username.length < 3) {
-      toast.error(
-        "Username should be greater than 3 characters.",
-        toastOptions
-      );
-      return false;
-    } else if (password.length < 8) {
-      toast.error(
-        "Password should be equal or greater than 8 characters.",
-        toastOptions
-      );
-      return false;
-    } else if (email === "") {
-      toast.error("Email is required.", toastOptions);
-      return false;
-    }
+  // const handleValidation = () => {
+  //   const { password, confirmPassword, username, email } = values;
+  //   if (password !== confirmPassword) {
+  //     toast.error(
+  //       "Password and confirm password should be same.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (username.length < 3) {
+  //     toast.error(
+  //       "Username should be greater than 3 characters.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (password.length < 8) {
+  //     toast.error(
+  //       "Password should be equal or greater than 8 characters.",
+  //       toastOptions
+  //     );
+  //     return false;
+  //   } else if (email === "") {
+  //     toast.error("Email is required.", toastOptions);
+  //     return false;
+  //   }
 
-    return true;
-  };
+  //   return true;
+  // };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (handleValidation()) {
-      const { email, username, password } = values;
-      const { data } = await axios.post(registerRoute, {
-        username,
-        email,
-        password,
-      });
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   if (handleValidation()) {
+  //     const { email, username, password } = values;
+  //     const { data } = await axios.post(registerRoute, {
+  //       username,
+  //       email,
+  //       password,
+  //     });
 
-      if (data.status === false) {
-        toast.error(data.msg, toastOptions);
-      }
-      if (data.status === true) {
-        localStorage.setItem(
-          process.env.REACT_APP_LOCALHOST_KEY,
-          JSON.stringify(data.user)
-        );
-        navigate("/");
-      }
-    }
-  };
+  //     if (data.status === false) {
+  //       toast.error(data.msg, toastOptions);
+  //     }
+  //     if (data.status === true) {
+  //       localStorage.setItem(
+  //         process.env.REACT_APP_LOCALHOST_KEY,
+  //         JSON.stringify(data.user)
+  //       );
+  //       navigate("/");
+  //     }
+  //   }
+  // };
+
+  // return (
+  //   <>
+  //     <FormContainer>
+  //       <form action="" onSubmit={(event) => handleSubmit(event)}>
+  //         <div className="brand">
+  //           <img src={Logo} alt="logo" />
+  //           <h1>snappy</h1>
+  //         </div>
+  //         <input
+  //           type="text"
+  //           placeholder="Username"
+  //           name="username"
+  //           onChange={(e) => handleChange(e)}
+  //         />
+  //         <input
+  //           type="email"
+  //           placeholder="Email"
+  //           name="email"
+  //           onChange={(e) => handleChange(e)}
+  //         />
+  //         <input
+  //           type="password"
+  //           placeholder="Password"
+  //           name="password"
+  //           onChange={(e) => handleChange(e)}
+  //         />
+  //         <input
+  //           type="password"
+  //           placeholder="Confirm Password"
+  //           name="confirmPassword"
+  //           onChange={(e) => handleChange(e)}
+  //         />
+  //         <button type="submit">Create User</button>
+  //         <span>
+  //           Already have an account ? <Link to="/login">Login.</Link>
+  //         </span>
+  //       </form>
+  //     </FormContainer>
+  //     <ToastContainer />
+  //   </>
+  // );
 
   return (
     <>
-      <FormContainer>
-        <form action="" onSubmit={(event) => handleSubmit(event)}>
-          <div className="brand">
-            <img src={Logo} alt="logo" />
-            <h1>snappy</h1>
+      <div className="content">
+        <div className="flex-div">
+          <div className="name-content">
+            <h1 className="logo">WinD</h1>
+            <p>Connect with friends and the world around you on Facebook.</p>
           </div>
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            onChange={(e) => handleChange(e)}
-          />
-          <button type="submit">Create User</button>
-          <span>
-            Already have an account ? <Link to="/login">Login.</Link>
-          </span>
-        </form>
-      </FormContainer>
-      <ToastContainer />
+          <form>
+            <input type="text" placeholder="Email or Phone Number" required="true" />
+            <input type="text" placeholder="Fullname" required="true" />
+            <input type="password" placeholder="Password" required="true" />
+            <input type="repassword" placeholder="Enter Password Again" required="true" />
+            <button className="login">Sign up</button>
+            <hr />
+            <Link to='/login'>Already have account? Login now</Link>
+          </form>
+        </div>
+      </div>
     </>
-  );
+  )
 }
 
 const FormContainer = styled.div`
